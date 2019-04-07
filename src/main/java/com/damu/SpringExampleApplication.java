d856b51c1328aaac272550c6f7b74e0b10ce92b1
damodara <pendeladamodhara@gmail.com>
2019-04-07 23:55:43 +0530

    first commit

---
+++ src/main/java/com/damu/SpringExampleApplication.java
@@ -0,0 +1,22 @@
+package com.damu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/")
+public class SpringExampleApplication {
+	private final static Logger LOGGER = LoggerFactory.getLogger(SpringExampleApplication.class);
+	public static void main(String[] args) {
+		
+		LOGGER.info("Before Starting of main method");
+		SpringApplication.run(SpringExampleApplication.class, args);
+		LOGGER.info("After Starting of main method");
+	}
+ 
+}
