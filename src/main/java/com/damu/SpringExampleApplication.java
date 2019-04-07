0609ad43881b62b805c338f46dc8557257c20a8b
damodara <pendeladamodhara@gmail.com>
2019-04-08 00:44:26 +0530

    added port

--- src/main/java/com/damu/SpringExampleApplication.java
+++ src/main/java/com/damu/SpringExampleApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,11 @@ public class SpringExampleApplication {
 		LOGGER.info("Before Starting of main method");
 		SpringApplication.run(SpringExampleApplication.class, args);
 		LOGGER.info("After Starting of main method");
+	}
+	@GetMapping("/message")
+	public String getWelcome()
+	{
+		return "Hello World";
 	}
 
 }
